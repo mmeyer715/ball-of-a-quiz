458890727bb9fa0dfecc7f620ae649110d865b7a
Maria Meyer <mbean1216@icloud.com>
2022-02-25 15:47:06 -0600

    trying to hide text

---
+++ script.js
@@ -0,0 +1,9 @@
+var timer = document.querySelector("count");
+var startBtn = document.querySelector("#start");
+
+// creating timer function
+function startTimer() {
+    timer.textContent = count;
+}
+
+// hide intro and start quiz
