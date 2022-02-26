8347e70808cc39126ec22f9e49625b90925a4ef3
Maria Meyer <mbean1216@icloud.com>
2022-02-26 11:56:16 -0600

    adding and removing classes as function)

--- script.js
+++ script.js
@@ -1,133 +1,156 @@
 //  pulling elements from html and creating their vars
 var timer = document.querySelector("count");
 var startBtn = document.querySelector("#start");
+var resultsBox = document.getElementById("#submit");
+var highscore = document.getElementById("#highscores");
 
 //  creating quiz questions
 var quizQuest = [
     {
-        questions: "JavaScript is a ________ ________.",
-        options: {
+        question: "JavaScript is a ________ ________.",
+        answers: {
             a: "scripting programming language",
             b: "object-oriented programming language",
             c: "procedural programming language",
             d: "logic programming language",
         },
-        ans: "a"
+        correct: "a"
     },
     {
-        questions: "Which of the following is not a promise object state?",
-        options: {
+        question: "Which of the following is not a promise object state?",
+        answers: {
             a: "rejected",
             b: "settled",
             c: "archived",
             d: "pending",
         },
-        ans: "c"
+        correct: "c"
     },
     {
-        questions: "Which of the following is a type of pop-up box in JavaScript?",
-        options: {
+        question: "Which of the following is a type of pop-up box in JavaScript?",
+        answers: {
             a: "confirm and",
             b: "prompt",
             c: "alert",
             d: "all of the above",
         },
-        ans: "d"
+        correct: "d"
     },
     {
-        questions: "What is a logical error?",
-        options: {
+        question: "What is a logical error?",
+        answers: {
             a: "Errors that come due to misuse of the command inside the HTML language.",
             b: "Errors that occur due to the bad logic performed ona function with a different operation.",
             c: "Errors  that come up when loading a web page, like improper syntax errors.",
             d: "Errors that occur when the user fails to provide a valid user name/password required for access to a file/directory.",
         },
-        ans: "b"
+        correct: "b"
     },
     {
-        questions: "Which of the following character types declare an array?",
-        options: {
+        question: "Which of the following character types declare an array?",
+        answers: {
             a: "var test = (];",
             b: "var test = ();",
             c: "var test = [];",
             d: "var test = {};",
         },
-        ans: "c"
+        correct: "c"
     },
     {
-        questions: "Which of the following is not a loop type in JavaScript?",
-        options: {
+        question: "Which of the following is not a loop type in JavaScript?",
+        answers: {
             a: "else loop",
             b: "while loop",
             c: "for loop",
             d: "do...while loop",
         },
-        ans: "a"
+        correct: "a"
     },
     {
-        questions: "What is the pop method for arrays in JavaScript?",
-        options: {
+        question: "What is the pop method for arrays in JavaScript?",
+        answers: {
             a: "You can add a value as the last item of an array",
             b: "You can remove the last item of an array",
             c: "You can sort the items in an array",
             d: "You can remove the first item of an array",
         },
-        ans: "b"
+        correct: "b"
     },
     {
-        questions: "Where do we put the JavaScript inside of the HTML?",
-        options: {
+        question: "Where do we put the JavaScript inside of the HTML?",
+        answers: {
             a: "<javascript>",
             b: "<js>",
             c: "<script>",
             d: "<java>",
         },
-        ans: "c"
+        correct: "c"
     },
     {
-        questions: "Which of the following methods is used to access HTML elements using JavaScript?",
-        options: {
+        question: "Which of the following methods is used to access HTML elements using JavaScript?",
+        answers: {
             a: ".getAttribute()",
             b: ".getElementsByClassName()",
             c: ".getElementById()",
             d: "both b and c",
         },
-        ans: "d"
+        correct: "d"
     },
     {
-        questions: "Which is not a data type in JavaScript?",
-        options: {
+        question: "Which is not a data type in JavaScript?",
+        answers: {
             a: "boolean",
             b: "valueless",
             c: "string",
             d: "undefined",
         },
-        ans: "b"
+        correct: "b"
     },
     {
-        questions: "What does the operator "===" translate to?",
-        options: {
+        question: "What does the operator "===" translate to?",
+        answers: {
             a: "returns the type of variable",
             b: "the values are equal only in value",
             c: "compares the inequalities of two operads.",
             d: "the values are equal in value and data type",
         },
-        ans: "d"
+        correct: "d"
     }
 ];
+
 
 // creating timer function
 function startTimer() {
     timer.textContent = count;
-}
+};
+
+
+// generating quiz questions
+function generateQuiz() {
+    // holds user output
+};
+// generating your results
+function yourResults() {
+    
+};
+
+// storing the to the leaderboard
+function leaderboard() {
+
+};
+
+generateQuiz();
 
 // hide intro and start quiz
 
-function hideIntro() {
-    var titleIntro = document.getElementById("container");
-    if (titleIntro.style.display === "none") {
-        titleIntro.style.display = "block";
-    } else {
-        titleIntro.style.display = "none";
-    }
-}    
+function hide() {
+    var hide = document.getElementById("hidden");
+    hide.classList.remove("intro");
+    hide.classList.add("intro.b h1 h3 button")
+    // var titleIntro = document.getElementsByClassName("intro");
+    // if (titleIntro.style.display === "none") {
+    //     titleIntro.style.display = "block";
+    // } else {
+    //     titleIntro.style.display = "none";
+    // }
+};
